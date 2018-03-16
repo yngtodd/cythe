ab67454604dfdc3ae8728ea29c402abce83bd368
Todd <myoung44@vols.utk.edu>
2018-03-16 10:15:34 -0400

    Spacing

--- src/demo.c
+++ src/demo.c
@@ -7,7 +7,6 @@ int scalar_int_add(int a, int b) {
     return c;
 }
 
-
 int np_int32_add(int32_t* a, int32_t* b, int32_t* out, int size) {
     for (int i = 0; i < size; ++i) {
         out[i] = a[i] + b[i];
